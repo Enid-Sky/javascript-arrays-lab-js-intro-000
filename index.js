77a035d1a2fe1a7c20b649f8cb6e952383bbd0d4
enid svymbersky <enid.bersky@gmail.com>
2019-11-24 19:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var kittens = ["Milo", "Otis", "Garfield"] ;//define your array here
 
-// Add your functions and code here
+// destructively append Ralph to the end of the array
   function destructivelyAppendKitten(name) {
   kittens.push("Ralph");
   return kittens;
 }
-
+// destructively append Bob to the beginning of the array
 function destructivelyPrependKitten(name){
   kittens.unshift("Bob");
   return kittens;
 }
-
+//destructively remove Garfield from the the array
 function destructivelyRemoveLastKitten(name){
   kittens.pop("Garfield");
   return kittens;
 }
-
+//destructively remove Milo from the array
 function destructivelyRemoveFirstKitten(name){
   kittens.shift("Milo");
   return kittens;
 }
-
+//simply append Broom to the beginning of the array and return a new array
 function appendKitten(name) {
   return [...kittens, "Broom"];
 }
