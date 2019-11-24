4545c23d446ba6a24601558e60567b4b0f5db446
enid svymbersky <enid.bersky@gmail.com>
2019-11-24 18:36:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,16 +16,5 @@ function destructivelyRemoveLastKitten(name){
   return kittens;
 }
 
-function appendKitten(name) {
-  kittens = (...kittens, "broom");
-  return kittens;
-}
 
-function destructivelyRemoveFirstKitten(name) {
-  kittens.shift("Milo");
-  return kittens;
-}
-function appendKitten(name) {
-  return (...kittens, "broom");
-}
 
